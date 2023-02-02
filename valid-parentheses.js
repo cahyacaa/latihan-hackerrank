9c8d9356fb254cfe33df943c49917b6ea02fdd94
cahyacaa <cahyanug3@gmail.com>
2023-02-02 18:00:58 +0700

    add valid parentheses problem

---
+++ valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let stackOfCloseParentheses = []
+    for (const char of s) {
+        if (char == "{") {
+            stackOfCloseParentheses.push("}")
+        } else if (char == "[") {
+            stackOfCloseParentheses.push("]")
+        } else if (char == '(') {
+            stackOfCloseParentheses.push(")")
+        } else { 
+            if (stackOfCloseParentheses.pop() != char){
+                return false
+            }
+        }
+
+    };
+    return true
+};
+
+console.log(isValid("{}]["))
