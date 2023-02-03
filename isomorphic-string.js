dd7692802e995dd5dc2677fd9ac3c39e90fe447b
cahyacaa <cahyanug3@gmail.com>
2023-02-03 20:35:04 +0700

    add new

---
+++ isomorphic-string.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+    let sMap = {}
+    let tMap = {}
+
+    let isNot = true
+
+    if (s.length != t.length) {
+        return false
+    }
+
+    for (let index = 0; index < s.length; index++) {
+        if (!isNot) {
+            continue
+        }
+
+        if (!sMap[s[index]]) {
+            sMap[s[index]] = t[index]
+        } else {
+            if (sMap[s[index]] !== t[index]) {
+                isNot = false
+            }
+        }
+        if (!tMap[t[index]]) {
+            tMap[t[index]] = s[index]
+        } else {
+            if (tMap[t[index]] !== s[index]) {
+                isNot = false
+            }
+        }
+    }
+
+    return isNot
+};
+
+
+console.log(isIsomorphic("paper", "title"))
