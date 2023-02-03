2899d6fc4f91258effde5be087aadfd4b5df9821
cahyacaa <cahyanug3@gmail.com>
2023-02-03 18:21:01 +0700

    update algothm

--- pivot-index.js
+++ pivot-index.js
@@ -5,26 +5,21 @@
 var pivotIndex = function (nums) {
     let rightSum = 0
     let leftSum = 0
-    let pivotI = 0
+    let pivotI = -1
     let count = 0
-    if (nums.length === 1) {
-        pivotI = 0
-        return pivotI
-    }
-    nums.forEach((_, i) => {
-        leftSum = sumWithInitial(nums.slice(0, i))
-        rightSum = sumWithInitial(nums.slice(-(nums.length - i) + 1))
-        if (i === nums.length - 1) {
-            rightSum = 0
-        }
-        if (leftSum === rightSum) {
-            count++
-            if (count < 2) {
+    rightSum = sumWithInitial(nums)
+
+    nums.forEach((e, i) => {
+        rightSum -= e
+        if (rightSum === leftSum) {
+            if (count < 1) {
                 pivotI = i
             }
+            count++
         }
+        leftSum += e
     })
-    return count == 0 ? -1 : pivotI
+    return pivotI
 
 };
 
@@ -35,4 +30,4 @@ const sumWithInitial = (number) => {
     );
 
 }
-console.log(pivotIndex([0, 0, 0, 0]))
+console.log(pivotIndex([-1,-1,0,0,-1,-1]))
